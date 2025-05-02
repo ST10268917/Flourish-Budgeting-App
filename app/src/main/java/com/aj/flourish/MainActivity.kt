b86832740d1fa29710e6f9a11c618ffaffea15e2
ST10268917 <ST10268917@vcconnect.edu.za>
2025-05-02 23:37:58 +0200

    Added code attribution

--- app/src/main/java/com/aj/flourish/MainActivity.kt
+++ app/src/main/java/com/aj/flourish/MainActivity.kt
@@ -36,6 +36,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
-
 }
+
+/*Code Attribution for the app
+CameraX implementation inspired by:
+Android Developers. n.d.-a. CameraX overview | Android media | Android Developers. [online] Available at: https://developer.android.com/training/camerax [Accessed 2 May 2025].
+UI elements or layout ideas potentially influenced by:
+Browse Fonts - Google Fonts. n.d. Browse Fonts - Google Fonts. [online] Available at: https://fonts.google.com/ [Accessed 2 May 2025].  * GitHub Actions workflow inspired by:
+automated-build-android-app-with-github-action:
+IMAD5112. n.d. Github-actions/.github/workflows/build.yml. [online] Available at: https://github.com/IMAD5112/Github-actions/blob/main/.github/workflows/build.yml [Accessed 2 May 2025]. */
